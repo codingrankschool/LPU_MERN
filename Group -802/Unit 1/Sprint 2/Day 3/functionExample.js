072863e7bae9aa9160973a6013021f47c73f4294
codingrankschool <jiraja2915@gmail.com>
2026-07-08 11:40:27 +0530

    added content and project allocation link

---
+++ Group -802/Unit 1/Sprint 2/Day 3/functionExample.js
@@ -0,0 +1,138 @@
+// Why? - Without function we are having to write duplicate code again and again which increases redudancy in your code.
+
+
+//Solution: Function -> Block of code related to specific functionality
+
+//declaration of function
+
+// function add(a, b){
+//     console.log(a + b)
+// }
+
+// add(12, 56) 
+
+// add(10, 34)
+
+
+//Analogy: Let's say you frequently use one application - Music Player
+
+//1. play music
+//2. puase music
+//3. switch to next music
+//3. increse volume
+
+
+// function playMusic(){
+//     console.log("Music started playing...")
+// }
+
+// function pauseMusic(){
+//     console.log("Music paused..")
+// }
+
+// function switchNextMusic(){
+//     console.log("Musice switched to another song")
+// }
+
+// function increaseVolumne(){
+//     console.log("Musice volumne Increased")
+// }
+
+// playMusic()
+// pauseMusic()
+// playMusic()
+
+
+//Note: function is reusable
+
+//Life cycle of function.
+
+//1. creation of function
+//2. call function
+//3. function exectutes
+//4. function returns something
+//5. function execution finished
+
+
+// //creation of function
+// function greeting(name, city){
+//     // console.log(name+"! Welcome to LPU at", city)
+//     console.log("Good Morning!")
+//     return name+"! Welcome to LPU at "+city
+//     console.log("Hello") //unreachable statement
+// }
+
+
+//calling function - by function name
+// greeting("Dinesh", "Phagwara")
+
+//parameters - function variables to use within function
+//argument - actual value you pass to function parameter(variable)
+
+//Note: function always returns something.
+
+// console.log(greeting("Dinesh", "Phagwara"))
+
+
+// function printArrayElement(arr){
+
+//     var squareArray = []
+
+//     for(var index in arr){
+//         squareArray.push(arr[index]**2)
+//     }
+
+//     return squareArray
+// }
+
+
+// var result = printArrayElement([1,2,3,4,5])
+// console.log(result)
+
+
+//Type of functions in javascript:
+/*
+1. Normal Function - Already discussed
+2. Anonymous Function(function expression)  - function without name
+3. Immediately Invoked Function Expression(IIFE)
+4. Callback function
+5. Higher Order Funciton
+6. Constructor Function
+7.Recursive Function 
+8. Arrow Function
+9. Generator Function
+*/
+
+//https://test.skilltrixa.com/login
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Group -802/Unit 1/Sprint 2/Day 3/stringExample.js
@@ -0,0 +1,25 @@
+//String: it is collection/set of characters(numbers, letters, specials sysmbols) enclosed withing single('') or double quotation("") marks
+
+// var str = "hello"
+//var favoriteCharacter = 'M'
+
+/*
+1. String is immutable(unchangable) - Why? Its contains very important data like adhaarNumber, acccountNumber, password, secretKey and others. 
+
+2. Like Array string also follows zero based indexing.
+*/
+
+// var str = "hello"
+
+// console.log(str) //hello
+// console.log(str[0])//h
+
+// //update or change character at index 0
+
+// str[0] = 't'
+
+// console.log(str)//hello
+
+
+//There are lots of inbuilt method of strings:
+//split, chatAt, toUpperCase, toLowerCase, startWith, endWith, slice, substring
