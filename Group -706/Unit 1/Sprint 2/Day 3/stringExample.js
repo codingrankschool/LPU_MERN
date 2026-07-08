03e15d3c415b4567c6e0a454d0209a86c16f495d
codingrankschool <jiraja2915@gmail.com>
2026-07-08 14:39:42 +0530

    added content and project allocation link

---
+++ Group -706/Unit 1/Sprint 2/Day 3/stringExample.js
@@ -0,0 +1,21 @@
+//Strings: It is collection/set of characters(numbers, letter, special symbols) enclosed within single quotation('') or double quotation("") marks
+
+
+var str = "hello"
+var fevoriteLetter = 'a'
+
+
+//Important Points
+/*
+1. String is immutable(unchangable) in javascript and even in other programming lagnauge as well.
+
+Why? - Its having most important and secure data therefore by default in all programming language string is immutable.
+
+Example: adhaarNumber, accountNumber, securePin, password, email and other data
+
+
+https://test.skilltrixa.com/login
+
+
+
+*/
